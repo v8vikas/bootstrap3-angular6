17194e650a2b6b250b5d40f9d21b25ef085493fc
v8vikas <v8.vikas@gmail.com>
2018-10-10 03:46:56 +0530

    - added router link for add message

--- src/app/components/messages/messages.module.ts
+++ src/app/components/messages/messages.module.ts
@@ -4,10 +4,12 @@ import { SharedModule } from '../shared/shared.module';
 import { AddMessageComponent } from './add-message/add-message.component';
 import { MessagesComponent } from './messages/messages.component';
 import { AddGroupMessageComponent } from './add-group-message/add-group-message.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
     CommonModule,
+    RouterModule,
     SharedModule
   ],
   declarations: [
