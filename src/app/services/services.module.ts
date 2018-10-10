672509587f09727d4ee89050d986d7e96b9d8f77
v8vikas <v8.vikas@gmail.com>
2018-10-11 01:38:03 +0530

    move the services in the individual forders

--- src/app/services/services.module.ts
+++ src/app/services/services.module.ts
@@ -1,9 +1,10 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { MessagesService } from './messages.service';
-import { UsersService } from './users.service';
-import { HttpService } from './http.service';
+import { HttpService } from './http/http.service';
+import { MessagesService } from './messages/messages.service';
+import { UsersService } from './users/users.service';
+
 
 @NgModule({
   imports: [
--- src/app/services/users/users.service.ts
+++ src/app/services/users/users.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpService } from './http.service';
 import { Observable } from 'rxjs/Observable';
+import { HttpService } from '../http/http.service';
 
 @Injectable()
 export class UsersService {
