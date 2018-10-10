01ecaa4d74c0c7d23ad0c90f399142ef2c46b92a
v8vikas <v8.vikas@gmail.com>
2018-10-11 01:56:30 +0530

    - Fixed the import errors

--- src/app/components/users/user-add/user-add.component.ts
+++ src/app/components/users/user-add/user-add.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
-import { UsersService } from '../../../services/users.service';
+import { UsersService } from '../../../services/users/users.service';
 
 @Component({
   selector: 'app-user-add',
--- src/app/services/http/http.service.ts
+++ src/app/services/http/http.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
-import { environment } from '../../environments/environment';
 import { Observable } from 'rxjs/Observable';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
 
 @Injectable()
 export class HttpService {
+
   baseUrl: string = environment.baseUrl;
   constructor(private http: HttpClient) { }
 
