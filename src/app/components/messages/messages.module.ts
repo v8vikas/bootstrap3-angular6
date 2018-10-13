4d0ef3c15ecc083c1eaea341ec778c840d4668fb
v8vikas <v8.vikas@gmail.com>
2018-10-14 01:39:48 +0530

    - Removed the add group message component as no longer required.

--- src/app/components/messages/messages.module.ts
+++ src/app/components/messages/messages.module.ts
@@ -3,26 +3,24 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { AddMessageComponent } from './add-message/add-message.component';
 import { MessagesComponent } from './messages/messages.component';
-import { AddGroupMessageComponent } from './add-group-message/add-group-message.component';
 import { RouterModule } from '@angular/router';
-import { NgxPaginationModule } from 'ngx-pagination'; // <-- import the module
+import { SendComponent } from './send/send.component';
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule,
-    SharedModule,
-    NgxPaginationModule
+    SharedModule
   ],
   declarations: [
     MessagesComponent,
     AddMessageComponent,
-    AddGroupMessageComponent
+    SendComponent
   ],
   exports: [
     MessagesComponent,
     AddMessageComponent,
-    AddGroupMessageComponent
+    SendComponent
   ]
 })
 export class MessagesModule { }
